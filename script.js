6fb0a7829487c1e26e5ef6e6e421616a9b60b179
x264j <149897750+x264j@users.noreply.github.com>
2023-12-01 17:14:30 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,54 @@
+var songNameInput = document.getElementById("songName");
+var suggestionsContainer = document.getElementById("suggestions");
+
+songNameInput.addEventListener("input", function() {
+    var songName = songNameInput.value;
+    getSuggestions(songName);
+});
+
+songNameInput.addEventListener("focus", function() {
+    var songName = songNameInput.value;
+    getSuggestions(songName);
+});
+
+function getSuggestions(songName) {
+    if (songName.length === 0) {
+        suggestionsContainer.innerHTML = "";
+        suggestionsContainer.style.display = "none";
+        return;
+    }
+
+    // Fetch song suggestions from music folder
+    fetch("getSongs.php?songName=" + songName)
+        .then(response => response.json())
+        .then(data => {
+            suggestionsContainer.innerHTML = "";
+            
+            data.forEach(songSuggestion => {
+                var li = document.createElement("li");
+                li.innerText = songSuggestion;
+                li.onclick = function() {
+                    songNameInput.value = this.innerText;
+                    suggestionsContainer.innerHTML = "";
+                    suggestionsContainer.style.display = "none";
+                };
+                
+                suggestionsContainer.appendChild(li);
+            });
+            
+            if (suggestionsContainer.childNodes.length > 0) {
+                suggestionsContainer.style.display = "block";
+            } else {
+                suggestionsContainer.style.display = "none";
+            }
+        })
+        .catch(error => {
+            console.error("Error fetching song suggestions:", error);
+        });
+}
+
 function playMusic() {
-    var songNameInput = document.getElementById("songName");
     var audioPlayer = document.getElementById("audioPlayer");
-    
     var songName = songNameInput.value;
     audioPlayer.src = "music/" + songName + ".mp3";
 }
